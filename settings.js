9cef9252d0216f93856f96f5a65b06b5a74bf4ea
Duc667 <enongaduc@gmail.com>
2025-02-01 07:40:23 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
-  author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  packname: 'D.U.C👑🖤',
+  author: 'D.U.C👑🖤',
+  botName: "👑Thorn👑",
+  botOwner: 'D.U.C👑🖤', // Your name
+  ownerNumber: '237697262077', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
